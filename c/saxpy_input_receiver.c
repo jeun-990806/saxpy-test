63a225fc5add28a7a6d3884ea8159449482a4e1e
jeun-990806 <kjeun990806@gmail.com>
2023-04-15 15:46:34 +0900

    add UDP option

--- c/saxpy_input_receiver.c
+++ c/saxpy_input_receiver.c
@@ -7,6 +7,9 @@
 #include <time.h>
 #include "tools.h"
 
+#define TCP 0
+#define UDP 1
+
 void saxpy(int n, float a, float *x, float *y){
         for(int i=0; i<n; i++)
                 y[i] = a*x[i]+y[i];
@@ -16,41 +19,75 @@ int main(int argc, char *argv[]){
 	int server_sock, client_sock;
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t client_addr_size;
+	char client_ip_address[INET_ADDRSTRLEN+1];
 
 	int n; float a;
 	float *x, *y;
 
-	server_sock = socket(PF_INET, SOCK_STREAM, 0);
+	int protocol = TCP;
+
+	if(strcmp(argv[2], "UDP") == 0) protocol = UDP;
+
+	if(protocol == TCP) server_sock = socket(PF_INET, SOCK_STREAM, 0);
+	else if(protocol == UDP) server_sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if(server_sock == -1){
+		printf("socket() error\n");
+		exit(1);
+	}
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(atoi(argv[1]));
 
-	bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-	listen(server_sock, 100);
+	if(bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+		printf("bind() error\n");
+		exit(1);
+	}
+	if(protocol == TCP) listen(server_sock, 100);
 	client_addr_size = sizeof(client_addr);
 
 	x = (float *)malloc(sizeof(float));
 	y = (float *)malloc(sizeof(float));
 	
 	while(1){
-		client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
-		printf("Connection request from %s\n" ntohl(client_addr.sin_addr.s_addr);
+		if(protocol == TCP){
+			client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
+			inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_ip_address, INET_ADDRSTRLEN);
+			printf("Connection request from %s\n", client_ip_address);
+		}
 
-		recv(client_sock, (void *)&n, sizeof(int), MSG_WAITALL);
-		if(n == -1) break;
-		recv(client_sock, (void *)&a, sizeof(float), MSG_WAITALL);
-		x = realloc(x, sizeof(float)*n);
-		y = realloc(y, sizeof(float)*n);
-		recv(client_sock, x, sizeof(float)*n, MSG_WAITALL);
-		recv(client_sock, y, sizeof(float)*n, MSG_WAITALL);
-		printf("Received data: n=%d, a=%f, x[]={%f, ..., %f}, y[]={%f, ..., %f} (Total: %dBytes)\n", n, a, x[0], x[n-1], y[0], y[n-1], read_bytes);
+		if(protocol == TCP){
+			recv(client_sock, (void *)&n, sizeof(int), MSG_WAITALL);
+			if(n == -1) break;
+			recv(client_sock, (void *)&a, sizeof(float), MSG_WAITALL);
+			x = realloc(x, sizeof(float)*n);
+			y = realloc(y, sizeof(float)*n);
+			recv(client_sock, x, sizeof(float)*n, MSG_WAITALL);
+			recv(client_sock, y, sizeof(float)*n, MSG_WAITALL);
+		}else if(protocol == UDP){
+			printf("Im waiting!\n");
+			recvfrom(server_sock, (void *)&n, sizeof(int), MSG_WAITALL, (struct sockaddr *)&client_addr, &client_addr_size);
+			if(n == -1) break;
+			recvfrom(server_sock, (void *)&a, sizeof(float), MSG_WAITALL, (struct sockaddr *)&client_addr, &client_addr_size);
+			
+			x = realloc(x, sizeof(float)*n);
+			y = realloc(y, sizeof(float)*n);
+			recvfrom(server_sock, x, sizeof(float)*n, MSG_WAITALL, (struct sockaddr *)&client_addr, &client_addr_size);
+			recvfrom(server_sock, y, sizeof(float)*n, MSG_WAITALL, (struct sockaddr *)&client_addr, &client_addr_size);
+		}
+		printf("Received data: n=%d, a=%f, x[]={%f, ..., %f}, y[]={%f, ..., %f}\n", n, a, x[0], x[n-1], y[0], y[n-1]);
 		
 		saxpy(n, a, x, y);
 		printf("saxpy() result: y[]={%f, ..., %f}\n", y[0], y[n-1]);
-
-		send(client_sock, y, sizeof(float)*n, 0);
-		if(close(client_sock) == 0) printf("Result reception is done. Disconnect.\n");
+		
+		if(protocol == TCP){
+			send(client_sock, y, sizeof(float)*n, 0);
+			if(close(client_sock) == 0)
+				printf("Result transmission is done. Disconnect.\n");
+		}else if(protocol == UDP){
+			sendto(server_sock, y, sizeof(float)*n, 0, (struct sockaddr *)&client_addr, client_addr_size);
+			printf("Result transmission is done.\n");
+		}
 	}
 	close(server_sock);
 	return 0;
