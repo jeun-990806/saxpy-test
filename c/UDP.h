f8b8dda2e4eddf92c3df843a7230921ac519bbaa
jeun-990806 <kjeun990806@gmail.com>
2023-04-16 16:00:49 +0900

    debug UDP.h

--- c/UDP.h
+++ c/UDP.h
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <sys/socket.h>
 
-ssize_t sendall(int socket, void *buffer, size_t length, size_t element_size, struct sockaddr *address, socklen_t *address_len){
+ssize_t sendall(int socket, void *buffer, size_t length, size_t element_size, struct sockaddr *address, socklen_t address_len){
 	ssize_t send_bytes = 0, total = 0;
+	int offset = 0;
 
 	while(1){
-		send_bytes = sendto(socket, buffer, length, address, address_len);
+		send_bytes = sendto(socket, buffer+offset, length, 0, address, address_len);
 		if(send_bytes == -1) return -1;
 		if(send_bytes + total >= length*element_size) return send_bytes + total;
 		total += send_bytes;
+		offset += send_bytes / element_size;
 	}
 }
 
-ssize_t recvall(int socket, void *buffer, size_t length, size_t element_size, struct sockaddr *address, socklen_t address_len){
-	ssize_t recv_bytes = 0; total = 0;
+ssize_t recvall(int socket, void *buffer, size_t length, size_t element_size, struct sockaddr *address, socklen_t *address_len){
+	ssize_t recv_bytes = 0, total = 0;
 	int offset = 0;
 	
 	while(1){
-		recv_bytes = recvfrom(socket, buffer + offset, length, address, address_len);
+		recv_bytes = recvfrom(socket, buffer+offset, length, 0, address, address_len);
 		if(recv_bytes == -1) return -1;
-		if(recv_bytes + total >= length*element_size) return recv_bytese + total;
+		if(recv_bytes + total >= length*element_size) return recv_bytes + total;
 		total += recv_bytes;
 		offset += recv_bytes / element_size;
 	}
